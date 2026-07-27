0d1ebaea72f4a62df3c1adda2a466254b5ef6baa
Ad Bazaar < GitHub Email>
2026-07-27 15:23:01 +0530

    Update sitemap for Ad Bazaar SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,10 +3,27 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
 
   return [
+
     {
-      url: "https://your-domain.com",
+      url: "https://adbazaar-website.vercel.app",
       lastModified: new Date(),
     },
+
+    {
+      url: "https://adbazaar-website.vercel.app/#services",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://adbazaar-website.vercel.app/#gallery",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://adbazaar-website.vercel.app/#contact",
+      lastModified: new Date(),
+    },
+
   ];
 
 }
