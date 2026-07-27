c4f9a7fdb6450257324db4d58f27dbcfde0276b6
Ad Bazaar < GitHub Email>
2026-07-27 15:59:33 +0530

    Update sitemap and robots for SEO

--- app/robots.ts
+++ app/robots.ts
@@ -3,19 +3,12 @@ import { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
 
   return {
-
     rules: {
-
       userAgent: "*",
-
       allow: "/",
-
     },
 
-
-    sitemap:
-      "https://adbazaar-website.vercel.app/sitemap.xml",
-
+    sitemap: "https://adbazaar-website.vercel.app/sitemap.xml",
   };
 
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,27 +3,10 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
 
   return [
-
     {
       url: "https://adbazaar-website.vercel.app",
       lastModified: new Date(),
     },
-
-    {
-      url: "https://adbazaar-website.vercel.app/#services",
-      lastModified: new Date(),
-    },
-
-    {
-      url: "https://adbazaar-website.vercel.app/#gallery",
-      lastModified: new Date(),
-    },
-
-    {
-      url: "https://adbazaar-website.vercel.app/#contact",
-      lastModified: new Date(),
-    },
-
   ];
 
 }
